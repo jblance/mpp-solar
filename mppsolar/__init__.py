e93f8707474d4433ea7bd82e65176f20d5c7518d
jblance <john.blance@gmail.com>
2017-09-08 09:33:37 +0000

    add options for status and settings

--- mppsolar/__init__.py
+++ mppsolar/__init__.py
@@ -12,7 +12,7 @@ logger = logging.getLogger()
 # if __name__ == '__main__':
 def main():
     parser = ArgumentParser(description='MPP Solar Command Utility')
-    parser.add_argument('command', help='Command to run')
+    parser.add_argument('-c', '--command', help='Command to run', default='QID')
     parser.add_argument('-ll', '--loglevel',
                         type=str,
                         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
@@ -20,6 +20,8 @@ def main():
     parser.add_argument('-d', '--device', type=str, help='Serial device to communicate with', default='/dev/ttyUSB0')
     parser.add_argument('-b', '--baud', type=int, help='Baud rate for serial communications', default=2400)
     parser.add_argument('-l', '--listknown', action='store_true', help='List known commands')
+    parser.add_argument('-s', '--getStatus', action='store_true', help='Get Inverter Status')
+    parser.add_argument('-t', '--getSettings', action='store_true', help='Get Inverter Settings')
     parser.add_argument('-H', '--makepretty', action='store_true', help='Display result with descriptions etc if possible')
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
@@ -32,6 +34,12 @@ def main():
 
     if(args.listknown):
         for line in mp.getKnownCommands():
+            print line
+    elif(args.getStatus):
+        for line in mp.getFullStatus():
+            print line
+    elif(args.getSettings):
+        for line in mp.getSettings():
             print line
     else:
         # TODO: check if command is valid
