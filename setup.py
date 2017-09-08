cfd01268d3c685e1dd77b0ccfb2e1fb58449e41b
jblance <1266998+jblance@users.noreply.github.com>
2017-09-08 14:58:33 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ https://github.com/pypa/sampleproject
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
-from codecs import open
+from codecs import open # pylint: disable=redefined-builtin
 from os import path
 
 here = path.abspath(path.dirname(__file__))
