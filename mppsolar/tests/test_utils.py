522026641ecf34d25d34da65307e4916dd8ae5b0
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 11:47:25 +1200

    Delete test_utils.py

--- mppsolar/tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from mppsolar import mpputils
-
-
-class test_utility(TestCase):
-    def testOne(self):
-        mp = mpputils.mppUtils('/dev/ttyUSB0')
-        mp.getKnownCommands()
-        return True
