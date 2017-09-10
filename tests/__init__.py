1d4dd60a933922d4aec8e94deff61da6a576881e
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 10:32:21 +1200

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,5 +8,6 @@ def load_tests(loader, tests, pattern):
     # Add core tests (from this package)
     core_tests_dir = os.path.dirname(os.path.abspath(__file__))
     tests.addTests(loader.discover(core_tests_dir, PATTERN))
+    print tests
 
     return tests
