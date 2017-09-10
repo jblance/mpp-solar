39e779b9346608958cc36a9061e232f46380a420
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 10:58:20 +1200

    Update test_mppcommands.py

--- tests/test_mppcommands.py
+++ tests/test_mppcommands.py
@@ -3,7 +3,7 @@ from mppcommands import mppCommands
 
 
 class test_mppcommands(unittest.TestCase):
-    def check_1(self):
+    def testOne(self):
         mp = mppCommands()
         mp.getKnownCommands()
         return True
