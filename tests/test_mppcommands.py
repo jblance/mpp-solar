3cf01f7c145f24b75241ede05a97e43aed636ed6
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 11:16:25 +1200

    Update test_mppcommands.py

--- tests/test_mppcommands.py
+++ tests/test_mppcommands.py
@@ -4,6 +4,6 @@ from mppsolar.mpputils import mppUtils
 
 class test_mppcommands(unittest.TestCase):
     def testOne(self):
-        mp = mpputils.mppUtils('/dev/ttyUSB0')
+        mp = mppUtils('/dev/ttyUSB0')
         mp.getKnownCommands()
         return True
