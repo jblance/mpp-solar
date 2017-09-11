f5822546dc74b85614f0267bb449783ac10f0314
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 12:58:20 +1200

    Update test_mppcommands.py

--- tests/test_mppcommands.py
+++ tests/test_mppcommands.py
@@ -5,7 +5,7 @@ from mppsolar import mppcommands
 class test_mppcommands(unittest.TestCase):
     def test_1(self):
         mp = mppcommands.mppCommands('/dev/ttyUSB0')
-        self.assertIsInstance(mp.getKnownCommands(), [])
+        self.assertIsInstance(mp.getKnownCommands(), array.array)
 
     def test_init1(self):
         """ Initialisation should fail if no device provided """
