def642c636272e14aa946efd347a1a25f547ff20
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 14:36:39 +1200

    Update test_mppcommands.py

--- tests/test_mppcommands.py
+++ tests/test_mppcommands.py
@@ -5,8 +5,14 @@ from mppsolar import mppcommands
 class test_mppcommands(unittest.TestCase):
     def test_trunc_function(self):
         """ Test trunc function correctly returns truncated / padded text """
-        self.assertEqual(mppcommands.trunc('Short text'), 'Short test                       ')
+        self.assertEqual(mppcommands.trunc('Short test'), 'Short test                       ')
         self.assertEqual(mppcommands.trunc('A very much looonnger test string'), 'A very much looonnger test str...')
+
+    def test_crc(self):
+        """ Test crc function generates correct crc """
+        self.assertListEqual(mppcommands.crc('QPIGS'), [183, 169])
+        self.assertListEqual(mppcommands.crc('QPIRI'), [248, 84])
+        self.assertListEqual(mppcommands.crc('PSDV56.4'), [249, 224])
 
     def test_failed_initialisation(self):
         """ Initialisation should fail if no device provided """
