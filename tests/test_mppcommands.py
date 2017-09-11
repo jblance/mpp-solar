1ace502b7c9e6825b215349f5b08176b77dcc3b3
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 14:29:36 +1200

    Update test_mppcommands.py

--- tests/test_mppcommands.py
+++ tests/test_mppcommands.py
@@ -3,6 +3,11 @@ from mppsolar import mppcommands
 
 
 class test_mppcommands(unittest.TestCase):
+    def test_trunc_function(self):
+        """ Test trunc function correctly returns truncated / padded text """
+        self.assertEqual(mppcommands.trunc('Short text'), 'Short test                       ')
+        self.assertEqual(mppcommands.trunc('A very much looonnger test string'), 'A very much looonnger test str...')
+
     def test_failed_initialisation(self):
         """ Initialisation should fail if no device provided """
         self.assertRaises(mppcommands.NoDeviceError, mppcommands.mppCommands)
