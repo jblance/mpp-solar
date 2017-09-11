e6181e3da7d5e3397554c527b5e35f2cf91a5033
jblance <1266998+jblance@users.noreply.github.com>
2017-09-11 13:12:15 +1200

    Update test_mppcommands.py

--- tests/test_mppcommands.py
+++ tests/test_mppcommands.py
@@ -5,8 +5,9 @@ from mppsolar import mppcommands
 
 class test_mppcommands(unittest.TestCase):
     def test_1(self):
+        known = ['-------- List of known commands --------', 'PBTnn: Set Battery Type', 'PSDVnn.n: Set Battery Cut-off Voltage', 'Q1: Q1 query', 'QBOOT: DSP Has Bootstrap inquiry', 'QDI: Device Default Settings inquiry', 'QDM: QDM query', 'QFLAG: Device Flag Status inquiry', 'QID: Device Serial Number inquiry', 'QMCHGCR: Max Charging Current Options inquiry', 'QMN: QMN query', 'QMUCHGCR: Max Utility Charging Current Options inquiry', 'QOPM: Output Mode inquiry', 'QPGSn: Parallel Information inquiry', 'QPI: Device Protocol ID inquiry', 'QPIGS: Device General Status Parameters inquiry', 'QPIRI: Device Current Settings inquiry', 'QPIWS: Device warning status inquiry', 'QVFW: Main CPU firmware version inquiry', 'QVFW2: Secondary CPU firmware version inquiry']
         mp = mppcommands.mppCommands('/dev/ttyUSB0')
-        self.assertIsInstance(mp.getKnownCommands(), array.array)
+        self.assertListEqual(IsInstance(mp.getKnownCommands(), known)
 
     def test_init1(self):
         """ Initialisation should fail if no device provided """
