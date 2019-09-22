074b7758fa405c09b5b6312da2ee471f5a518da5
jblance <1266998+jblance@users.noreply.github.com>
2019-09-23 10:59:33 +1200

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,10 +4,10 @@ import unittest
 
 def get_tests():
 
-    from .test_mppcommands import test_mppcommands
+    from .test_mppinverter import test_mppinverter
     from .test_mpputils import test_mpputils
 
-    mppcommands = unittest.TestLoader().loadTestsFromTestCase(test_mppcommands)
+    mppinverter = unittest.TestLoader().loadTestsFromTestCase(test_mppinverter)
     mpputils = unittest.TestLoader().loadTestsFromTestCase(test_mpputils)
 
-    return unittest.TestSuite([mppcommands, mpputils])
+    return unittest.TestSuite([mppinverter, mpputils])
