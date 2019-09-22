c1afe3053ef79ae0ae857a9ac43be10a3693636b
jblance <1266998+jblance@users.noreply.github.com>
2019-09-23 10:57:09 +1200

    Update test_mpputils.py

--- tests/test_mpputils.py
+++ tests/test_mpputils.py
@@ -1,9 +1,9 @@
 import unittest
 from mppsolar import mpputils
-from mppsolar import mppcommands
+from mppsolar import mppinverter
 
 
 class test_mpputils(unittest.TestCase):
     def test_failed_initialisation(self):
         """ Initialisation should fail if no device provided """
-        self.assertRaises(mppcommands.NoDeviceError, mpputils.mppUtils)
+        self.assertRaises(mppinverter.NoDeviceError, mpputils.mppUtils)
