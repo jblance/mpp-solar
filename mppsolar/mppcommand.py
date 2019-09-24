f919a82e447840c8fdf9562c566fd63070b229d2
jblance <1266998+jblance@users.noreply.github.com>
2019-09-24 15:14:13 +1200

    Update mppcommand.py
    Treat each flag as a separate message from ltowarek

--- mppsolar/mppcommand.py
+++ mppsolar/mppcommand.py
@@ -203,12 +203,16 @@ class mppCommand(object):
             elif (resp_format[0] == 'keyed'):
                 msgs[key] = [resp_format[2][result], '']
             # eg. ['flags', 'Device status', [ ['Add SBU - No', 'Add SBU - ...
+            # eg. ['flags', 'Device status', [ 'is_load_on', 'is_charging_on' ...
             elif (resp_format[0] == 'flags'):
-                output = ''
                 for j, flag in enumerate(result):
-                    output = '{}\n\t- {}'.format(output,
-                                                 resp_format[2][j][int(flag)])
-                msgs[key] = [output, '']
+                    msgs[resp_format[2][j]] = [int(flag), 'True - 1/False - 0']
+            # elif (resp_format[0] == 'flags'):
+                # output = ''
+                # for j, flag in enumerate(result):
+                    # output = '{}\n\t- {}'.format(output,
+                    #                             resp_format[2][j][int(flag)])
+                # msgs[key] = [output, '']
             # eg. ['stat_flags', 'Warning status', ['Reserved', 'Inver...
             elif (resp_format[0] == 'stat_flags'):
                 output = ''
