3a3561157f4a98c926e62809ea83184f540797ab
jblance <1266998+jblance@users.noreply.github.com>
2019-09-24 14:12:43 +1200

    Update test_mpputils.py

--- tests/test_mpputils.py
+++ tests/test_mpputils.py
@@ -7,3 +7,10 @@ class test_mpputils(unittest.TestCase):
     def test_failed_initialisation(self):
         """ Initialisation should fail if no device provided """
         self.assertRaises(mppinverter.NoDeviceError, mpputils.mppUtils)
+
+    def test_mpputils_init_test(self):
+        """ test initialisation of TEST connection"""
+        utils = mpputils.mppUtils('TEST')
+        # command = inverter.execute('QPIWS')
+        # print(command)
+        self.assertIsInstance(utils, mpputils.mppUtils)
