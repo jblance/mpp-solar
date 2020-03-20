3b372d4c9bb97ac4f2628d1d22eb7fc4b15784d4
John Blance <john.blance@gmail.com>
2020-03-20 13:48:56 +1300

    add info level message option to arguments

--- mppsolar/__init__.py
+++ mppsolar/__init__.py
@@ -11,7 +11,8 @@ log = logging.getLogger('MPP-Solar')
 def main():
     parser = ArgumentParser(description='MPP Solar Command Utility')
     parser.add_argument('-c', '--command', help='Command to run', default='QID')
-    parser.add_argument('-D', '--enableDebug', action='store_true', help='Enable Debug')
+    parser.add_argument('-D', '--enableDebug', action='store_true', help='Enable Debug and above (i.e. all) messages')
+    parser.add_argument('-I', '--enableInfo', action='store_true', help='Enable Info and above level messages')
     parser.add_argument('-d', '--device', type=str, help='Serial device to communicate with', default='/dev/ttyUSB0')
     parser.add_argument('-b', '--baud', type=int, help='Baud rate for serial communications', default=2400)
     parser.add_argument('-l', '--listknown', action='store_true', help='List known commands')
@@ -20,6 +21,7 @@ def main():
     parser.add_argument('-R', '--showraw', action='store_true', help='Display the raw results')
     args = parser.parse_args()
 
+    # setup logging (DEBUG, INFO, WARNING, ERROR, CRITICAL)
     ch = logging.StreamHandler()
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(filename)s - %(levelname)s - %(message)s')
@@ -28,14 +30,17 @@ def main():
     if(args.enableDebug):
         log.setLevel(logging.DEBUG)
         ch.setLevel(logging.DEBUG)
+    elif(args.enableInfo):
+            log.setLevel(logging.INFO)
+            ch.setLevel(logging.INFO)
     else:
         log.setLevel(logging.WARNING)
         ch.setLevel(logging.WARNING)
     # add the handlers to logger
     log.addHandler(ch)
 
-    log.debug('command %s', args.command)
-    log.debug('Serial device used: %s, baud rate: %d', args.device, args.baud)
+    log.info('command %s', args.command)
+    log.info('Serial device used: %s, baud rate: %d', args.device, args.baud)
 
     # mp = mppcommands.mppCommands(args.device, args.baud)
     mp = mppUtils(args.device, args.baud)
