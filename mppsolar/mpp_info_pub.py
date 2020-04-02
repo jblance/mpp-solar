89c3f710856c43b22d6900a313a547bbf1efec52
John Blance <john.blance@gmail.com>
2020-04-03 11:31:43 +1300

    change lookup

--- mppsolar/mpp_info_pub.py
+++ mppsolar/mpp_info_pub.py
@@ -55,10 +55,10 @@ def main():
                     # 92931509101901/status/total_output_active_power/value 1250
                     # 92931509101901/status/total_output_active_power/unit W
                     topic = '{}/status/{}/value'.format(serial_number, _item)
-                    msg = {'topic': topic, 'payload': '{}'.format(_item[0])}
+                    msg = {'topic': topic, 'payload': '{}'.format(_data[_item][0])}
                     msgs.append(msg)
                     topic = '{}/status/{}/unit'.format(serial_number, _item)
-                    msg = {'topic': topic, 'payload': '{}'.format(_item[1])}
+                    msg = {'topic': topic, 'payload': '{}'.format(_data[_item][1])}
                     msgs.append(msg)
             publish.multiple(msgs, hostname=args.broker)
         # Collect Inverter Status data and publish
