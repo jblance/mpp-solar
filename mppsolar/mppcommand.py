6fb2957a5642d93fc097ffb7794c6480d176f52f
John Blance <john.blance@gmail.com>
2020-05-03 21:42:57 +1200

    shorten resp

--- mppsolar/mppcommand.py
+++ mppsolar/mppcommand.py
@@ -162,7 +162,7 @@ class mppCommand(object):
             log.debug('Byte Response invalid as too short')
             return False
         # Check we got a CRC byte_response that matches the data
-        resp = byte_response[:-3]
+        resp = byte_response[1:-3]
         resp_crc = byte_response[-3:-1]
         log.debug('CRC resp\t {}, {}'.format(resp_crc[0], resp_crc[1]))
         calc_crc_h, calc_crc_l = crc(resp)
