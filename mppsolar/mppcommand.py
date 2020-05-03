12fcfb469df837c6844dc4d41b360031bc9da56d
John Blance <john.blance@gmail.com>
2020-05-03 21:25:18 +1200

    remove fromhex

--- mppsolar/mppcommand.py
+++ mppsolar/mppcommand.py
@@ -161,7 +161,7 @@ class mppCommand(object):
         # Check we got a CRC byte_response that matches the data
         resp = byte_response[:-3]
         resp_crc = byte_response[-3:-1]
-        log.debug('CRC resp\t {}, {}'.format(bytes.fromhex(resp_crc[0]), resp_crc[1]))
+        log.debug('CRC resp\t {}, {}'.format(resp_crc[0], resp_crc[1]))
         calc_crc_h, calc_crc_l = crc(resp)
         log.debug('CRC calc\t', calc_crc_h, calc_crc_l)
         if ((resp_crc[0] == calc_crc_h) and (resp_crc[1] == calc_crc_l)):
