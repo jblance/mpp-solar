0ad3938b1549cf3d211a6ef330a9fa3b96ee3007
John Blance <john.blance@gmail.com>
2020-07-08 14:09:33 +1200

    further __init__ refactor

--- mppsolar/__init__.py
+++ mppsolar/__init__.py
@@ -1,7 +1,8 @@
 # !/usr/bin/python3
 from argparse import ArgumentParser
+import importlib
 import logging
-import sys
+from sys import exit
 
 from .version import __version__  # noqa: F401
 # import mppcommands
@@ -24,18 +25,18 @@ logging.basicConfig()
 def main():
     parser = ArgumentParser(description='MPP Solar Command Utility, version: {}'.format(__version__))
     parser.add_argument('-n', '--name', type=str, help='Specifies the device name - used to differentiate different devices', default='mppsolarname')
-    parser.add_argument('-t', '--type', type=str, help='Specifies the device type (mppsolar [default], jkbms)', default='mppsolar')
-    parser.add_argument('-d', '--device', type=str, help='Specifies the device to communicate with (/dev/ttyUSB0 [default], /dev/hidraw, test, ...)', default='/dev/ttyUSB0')
-    # parser.add_argument('-p', '--port', type=str, help='Specifies the device communication port, (/dev/ttyUSB0 [default], /dev/hidraw0, test ...)', default='/dev/ttyUSB0')
+    parser.add_argument('-t', '--type', type=str, help='Specifies the device type (default: mppsolar)', default='mppsolar')
+    parser.add_argument('-p', '--port', type=str, help='Specifies the device communications port (/dev/ttyUSB0 [default], /dev/hidraw, test, ...)', default='/dev/ttyUSB0')
+    parser.add_argument('-d', '--device', type=str, help='DEPRICATED, use -p')
     parser.add_argument('-P', '--protocol', type=str, help='Specifies the device command and response protocol, (default: PI30)', default='PI30', choices=['PI18', 'PI30', 'PI41'])
     parser.add_argument('-T', '--tag', type=str, help='Override the command name and use this instead (for mqtt and influx type output processors)')
-    parser.add_argument('-b', '--baud', type=int, help='Baud rate for serial communications, defaults to 2400', default=2400)
+    parser.add_argument('-b', '--baud', type=int, help='Baud rate for serial communications (default: 2400)', default=2400)
     parser.add_argument('-M', '--model', type=str, help='Specifies the inverter model to select commands for, defaults to "standard", currently supports LV5048', default='standard')
 
     parser.add_argument('-c', '--command', help='Command to run', default='QID')
     # parser.add_argument('-c', '--command', help='Raw command to run')
 
-    parser.add_argument('-o', '--output', type=str, help='Specifies the output processor(s) to use [comma separated if multiple] (screen [default], influx_mqtt, mqtt, hass_config, hass_mqtt)', default='screen')
+    parser.add_argument('-o', '--output', type=str, help='Specifies the output processor(s) to use [comma separated if multiple] (default: screen)', choices=['screen', 'influx_mqtt', 'mqtt', 'hass_config', 'hass_mqtt'], default='screen')
     parser.add_argument('-q', '--mqtt_broker', type=str, help='Specifies the mqtt broker to publish to if using a mqtt output (localhost [default], hostname, ip.add.re.ss ...)', default='localhost')
 
     parser.add_argument('-l', '--listknown', action='store_true', help='List known commands')
@@ -44,16 +45,16 @@ def main():
     parser.add_argument('--getSettings', action='store_true', help='Get Inverter Settings')
 
     parser.add_argument('-R', '--showraw', action='store_true', help='Display the raw results')
-    parser.add_argument('-D', '--enableDebug', action='store_true', help='Enable Debug and above (i.e. all) messages')
-    parser.add_argument('-I', '--enableInfo', action='store_true', help='Enable Info and above level messages')
-    parser.add_argument('-p', '--printcrc', action='store_true', help='Display the command and crc and nothing else')
+    parser.add_argument('-D', '--debug', action='store_true', help='Enable Debug and above (i.e. all) messages')
+    parser.add_argument('-I', '--info', action='store_true', help='Enable Info and above level messages')
+    parser.add_argument('--printcrc', action='store_true', help='Display the command and crc and nothing else')
     args = parser.parse_args()
 
     # Turn on debug if needed
-    if(args.enableDebug):
+    if(args.debug):
         log.setLevel(logging.DEBUG)
         # ch.setLevel(logging.DEBUG)
-    elif(args.enableInfo):
+    elif(args.info):
         log.setLevel(logging.INFO)
         # ch.setLevel(logging.INFO)
 
@@ -68,80 +69,78 @@ def main():
         mqtt_broker = args.mqtt_broker
     else:
         mqtt_broker = 'localhost'
-    if args.baud:
-        log.error('ERROR: baud option is deprecated please update your scripts')
-    if(args.listknown):
+    if args.listknown:
         log.error('-l --listknown option is deprecated, please update your scripts')
-        sys.exit(1)
-
-    log.debug(f'communications device used: {args.device}, tag: {tag}, mqtt_broker: {mqtt_broker}')
-
+        exit(1)
+    if args.device:
+        log.error('-d --device option is deprecated, please update your scripts to use -p instead')
+        args.port = args.device
+    if args.printcrc:
+        log.info(f'Calculating CRC using protocol {args.protocol}')
+        # TODO: calc CRC
+        # _command = mp.getFullCommand(args.command)
+        # if _command:
+        #     print('{}'.format(_command.byte_command))
+        # else:
+        #     [crca, crcb] = mppcommand.crc(args.command)  # noqa: F821
+        #     print("{0} {1:#x} {2:#x}".format(args.command, crca, crcb))
+        exit(1)
     # create instance of device (supplying port + protocol types)
-    # log.info(f'Creating device "{args.name}" (type: "{args.type}") on port "{args.port}" using protocol "{args.protocol}"')
-    # device_type = args.type.lower()
-    # try:
-    #     device_module = importlib.import_module('powermon.devices.' + device_type, '.')
-    # except ModuleNotFoundError:
-    #     # perhaps raise a Powermon exception here??
-    #     log.critical(f'No module found for device {device_type}')
-    #     exit(1)
-    # device_class = getattr(device_module, device_type)
-    # log.debug(f'device_class {device_class}')
-    # # The device class __init__ will instantiate the port communications and protocol classes
-    # device = device_class(name=args.name, port=args.port, protocol=args.protocol)
-    log.debug(f'command {args.command}')
+    log.info(f'Creating device "{args.name}" (type: "{args.type}") on port "{args.port}" using protocol "{args.protocol}" for command "{args.command}" (tag: {tag})')
+    device_type = args.type.lower()
+    try:
+        device_module = importlib.import_module('mppsolar.devices.' + device_type, '.')
+    except ModuleNotFoundError:
+        # perhaps raise a mppsolar exception here??
+        log.critical(f'No module found for device {device_type}')
+        exit(1)
+    device_class = getattr(device_module, device_type)
+    log.debug(f'device_class {device_class}')
+    # The device class __init__ will instantiate the port communications and protocol classes
+    device = device_class(name=args.name, port=args.port, protocol=args.protocol)
+    # run command or call helper function
+    results = device.run_command(command=args.command, show_raw=args.show_raw)
+
+    #
     # mp = mppcommands.mppCommands(args.device, args.baud)
-    mp = mppUtils(args.device, args.baud, args.model)
-
-    if(args.printcrc):
-        # print("{0:#x}".format(100))
-        _command = mp.getFullCommand(args.command)
-        if _command:
-            print('{}'.format(_command.byte_command))
-        else:
-            [crca, crcb] = mppcommand.crc(args.command)  # noqa: F821
-            print("{0} {1:#x} {2:#x}".format(args.command, crca, crcb))
-
-    elif(args.getStatus):
-        fullStatus = mp.getFullStatus()
-        print("================ Status ==================")
-        print("{:<30}\t{:<15} {}".format('Parameter', 'Value', 'Unit'))
-        for key in sorted(fullStatus):
-            print("{:<30}\t{:<15} {}".format(key, fullStatus[key]['value'], fullStatus[key]['unit']))
-    elif(args.getSettings):
-        settings = mp.getSettings()
-        print("================ Settings ==================")
-        print("{:<30}\t{:<10}\t{:<10} {}".format('Parameter', 'Default', 'Current', 'Unit'))
-        for key in sorted(settings):
-            print("{:<30}\t{:<10}\t{:<10} {}".format(key, settings[key]['default'],
-                                                     settings[key]['value'],
-                                                     settings[key]['unit']))
-    else:
-        # TODO: check if command is valid
-        # maybe check if query or setter and ...
-        if(args.showraw):
-            print(mp.getResponse(args.command))
-        else:
-            results = mp.getResponseDict(args.command)
-            for key in sorted(results):
-                print("{:<30}\t{:<15} {}".format(key, results[key][0], results[key][1]))
-
     #
     #
-    # # run command or called helper function
-    # results = device.run_command(command=args.command, show_raw=args.show_raw)
-    #
+    # mp = mppUtils(args.device, args.baud, args.model)
 
-    # # send to output processor(s)
-    # outputs = args.output.split(',')
-    # for output in outputs:
-    #     log.info(f'attempting to create output processor: {output}')
-    #     try:
-    #         output_module = importlib.import_module('powermon.outputs.' + output, '.')
-    #     except ModuleNotFoundError:
-    #         # perhaps raise a Powermon exception here??
-    #         log.critical(f'No module found for output processor {output}')
-    #     output_class = getattr(output_module, output)
-    #
-    #     # init function will do the processing
-    #     output_class(results=results, tag=tag, mqtt_broker=mqtt_broker)
+    # elif(args.getStatus):
+    #     fullStatus = mp.getFullStatus()
+    #     print("================ Status ==================")
+    #     print("{:<30}\t{:<15} {}".format('Parameter', 'Value', 'Unit'))
+    #     for key in sorted(fullStatus):
+    #         print("{:<30}\t{:<15} {}".format(key, fullStatus[key]['value'], fullStatus[key]['unit']))
+    # elif(args.getSettings):
+    #     settings = mp.getSettings()
+    #     print("================ Settings ==================")
+    #     print("{:<30}\t{:<10}\t{:<10} {}".format('Parameter', 'Default', 'Current', 'Unit'))
+    #     for key in sorted(settings):
+    #         print("{:<30}\t{:<10}\t{:<10} {}".format(key, settings[key]['default'],
+    #                                                  settings[key]['value'],
+    #                                                  settings[key]['unit']))
+    # else:
+    #     # TODO: check if command is valid
+    #     # maybe check if query or setter and ...
+    #     if(args.showraw):
+    #         print(mp.getResponse(args.command))
+    #     else:
+    #         results = mp.getResponseDict(args.command)
+    #         for key in sorted(results):
+    #             print("{:<30}\t{:<15} {}".format(key, results[key][0], results[key][1]))
+
+    # send to output processor(s)
+    outputs = args.output.split(',')
+    for output in outputs:
+        log.info(f'attempting to create output processor: {output}')
+        try:
+            output_module = importlib.import_module('mppsolar.outputs.' + output, '.')
+        except ModuleNotFoundError:
+            # perhaps raise a Powermon exception here??
+            log.critical(f'No module found for output processor {output}')
+        output_class = getattr(output_module, output)
+
+        # init function will do the processing
+        output_class(results=results, tag=tag, mqtt_broker=mqtt_broker)
