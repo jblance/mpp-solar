aed410319e33b9ccb2445239b57ca6ec1e53bd2d
John Blance <john.blance@gmail.com>
2020-07-09 14:35:32 +1200

    update screen output _data -> data

--- mppsolar/outputs/screen.py
+++ mppsolar/outputs/screen.py
@@ -7,12 +7,12 @@ class screen():
     def __init__(self, *args, **kwargs) -> None:
         log.debug(f'processor.screen __init__ kwargs {kwargs}')
 
-    def output(_data=None):
+    def output(data=None):
         log.info('Using output processor: screen')
-        if _data is None:
+        if data is None:
             return
         print(f"{'Parameter':<30}\t{'Value':<15} Unit")
-        for key in _data:
-            value = _data[key][0]
-            unit = _data[key][1]
+        for key in data:
+            value = data[key][0]
+            unit = data[key][1]
             print(f'{key:<30}\t{value:<15}\t{unit:<4}')
