670b8bb1ae73e2ffb47b10e87ad76f4827c8160f
John Blance <john.blance@gmail.com>
2020-12-14 20:03:34 +1300

    add paho-mqtt to install requires

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["pyserial"],
+    install_requires=["pyserial", "paho-mqtt"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
