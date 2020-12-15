7e0b8485b14ea3f476d13ea441470edc2fa11ef9
John Blance <john.blance@gmail.com>
2020-12-16 12:07:55 +1300

    version bump

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,4 +1,2 @@
-__version__ = "0.7.4"
-__version_comment__ = (
-    "Refactored version - not backwards compatible, refactor mpp-solar-service"
-)
+__version__ = "0.7.5"
+__version_comment__ = "Refactored version - not backwards compatible, add jk24s device"
