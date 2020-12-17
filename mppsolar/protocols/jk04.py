80cfa97b5a20bb37d5fd2a5d0f00066bac00458a
John Blance <john.blance@gmail.com>
2020-12-18 12:52:26 +1300

    add asc type to jk04

--- mppsolar/protocols/jk04.py
+++ mppsolar/protocols/jk04.py
@@ -35,6 +35,9 @@ COMMANDS = {
             ["hex", 4, "Header", ""],
             ["hex", 1, "Record Type", ""],
             ["int", 1, "Record Counter", ""],
+            ["asc", 10, "Device Model", ""],
+            ["asc", 10, "Hardware Version", ""],
+            ["asc", 10, "Software Version", ""],
         ],
         "test_responses": [
             bytes.fromhex(
@@ -144,8 +147,16 @@ class jk04(AbstractProtocol):
                 if defn[0] == "hex":
                     value = ""
                     for x in range(defn[1]):
-                        print(f"value {value}")
                         value += f"{responses.pop(0):02x}"
+                    msgs[defn[2]] = [value, ""]
+                elif defn[0] == "asc":
+                    value = ""
+                    for x in range(defn[1]):
+                        b = responses.pop(0)
+                        if b == 0:
+                            continue
+                        value += f"{b:c}"
+                        print(f"value: {value}")
                     msgs[defn[2]] = [value, ""]
                 elif defn[0] == "int":
                     msgs[defn[2]] = [responses.pop(0), ""]
