ff62c6b2510a802845be47809333c53ff4efcec0
John Blance <john.blance@gmail.com>
2020-12-18 22:26:38 +1300

    version bump

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.7.7"
-__version_comment__ = "Refactored version - not backwards compatible, add jkbms support"
+__version__ = "0.7.8"
+__version_comment__ = "Refactored version - not backwards compatible, add jkbms (02 and 04) support"
