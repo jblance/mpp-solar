242f2acd6680d39f68827932b117c384f3822f65
John Blance <john.blance@gmail.com>
2021-01-11 09:24:33 +1300

    remove unused loop in serialIO

--- mppsolar/io/serialio.py
+++ mppsolar/io/serialio.py
@@ -24,25 +24,23 @@ class SerialIO(BaseIO):
         log.debug(f"port {self._serial_port}, baudrate {self._serial_baud}")
         try:
             with serial.serial_for_url(self._serial_port, self._serial_baud) as s:
-                # Execute command multiple times, increase timeouts each time
-                for x in range(1, 5):
-                    log.debug(f"Command execution attempt {x}...")
-                    s.timeout = 1 + x
-                    s.write_timeout = 1 + x
-                    s.flushInput()
-                    s.flushOutput()
-                    s.write(full_command)
-                    time.sleep(0.1 * x)  # give serial port time to receive the data
-                    response_line = s.read_until(expected=b'\r')
-                    log.debug("serial response was: %s", response_line)
-                    decoded_response = protocol.decode(response_line, show_raw)
-                    log.debug(f"Decoded response {decoded_response}")
-                    # add command name and description to response
-                    decoded_response["_command"] = command
-                    if command_defn is not None:
-                        decoded_response["_command_description"] = command_defn["description"]
-                    log.info(f"Decoded response {decoded_response}")
-                    return decoded_response
+                log.debug(f"Executing command via serialio...")
+                s.timeout = 1
+                s.write_timeout = 1
+                s.flushInput()
+                s.flushOutput()
+                s.write(full_command)
+                time.sleep(0.1)  # give serial port time to receive the data
+                response_line = s.read_until(expected=b"\r")
+                log.debug("serial response was: %s", response_line)
+                decoded_response = protocol.decode(response_line, show_raw)
+                log.debug(f"Decoded response {decoded_response}")
+                # add command name and description to response
+                decoded_response["_command"] = command
+                if command_defn is not None:
+                    decoded_response["_command_description"] = command_defn["description"]
+                log.info(f"Decoded response {decoded_response}")
+                return decoded_response
         except Exception as e:
             log.warning(f"Serial read error: {e}")
         log.info("Command execution failed")
