9b772467670458452d9160d19ef08046470f3b83
John Blance <john.blance@gmail.com>
2021-01-11 08:34:49 +1300

    version bump

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,2 +1,4 @@
-__version__ = "0.7.8"
-__version_comment__ = "Refactored version - not backwards compatible, add jkbms (02 and 04) support"
+__version__ = "0.7.9"
+__version_comment__ = (
+    "recent changes: jkbms (02, 04 485) support, merge PR with fixes and serialio improvements"
+)
