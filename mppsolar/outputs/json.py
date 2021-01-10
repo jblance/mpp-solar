db3e47e9265f09e669840bbc58f9cf3a7f1f8931
John Blance <john.blance@gmail.com>
2021-01-11 10:53:21 +1300

    add json output

---
+++ mppsolar/outputs/json.py
@@ -0,0 +1,17 @@
+import logging
+
+log = logging.getLogger("MPP-Solar")
+
+
+class json:
+    def __init__(self, *args, **kwargs) -> None:
+        log.debug(f"processor.json __init__ kwargs {kwargs}")
+
+    def output(self, data=None, tag=None, mqtt_broker="localhost"):
+        log.info("Using output processor: json")
+        output = {}
+        for key in data:
+            value = data[key][0]
+            # unit = data[key][1]
+            output[key] = value
+        print(output)
