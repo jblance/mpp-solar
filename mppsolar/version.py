8d318cd1f31c32d957b9d2c9e4a7283f5650c721
John Blance <john.blance@gmail.com>
2021-01-13 10:06:50 +1300

    version bump

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.7.15"
-__version_comment__ = "recent changes: remove JKBLE temporarily"
+__version__ = "0.7.16"
+__version_comment__ = "recent changes: re-add JKBLE after separating jkbms bin"
