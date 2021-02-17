104b2179d5b07e2aa12b674e92b7f33d1ec736f1
John Blance <john.blance@gmail.com>
2021-02-17 13:47:12 +1300

    version bump

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.7.36"
-__version_comment__ = "recent changes: vedirect changed to ved"
+__version__ = "0.7.37"
+__version_comment__ = "recent changes: update ved work, add a get command to ved"
