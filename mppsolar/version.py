b77e04ca91580004615190c89be3fcc112b10aa7
John Blance <john.blance@gmail.com>
2021-04-14 09:46:46 +1200

    version bump

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.7.41"
-__version_comment__ = "recent changes: add daly (rs485) protocol"
+__version__ = "0.7.42"
+__version_comment__ = "recent changes: deal with non-numerical result better "
