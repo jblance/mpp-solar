06bd14cbd09575b7d746f547f8f03f9593505b44
John Blance <john.blance@gmail.com>
2021-05-14 13:29:48 +1200

    extra pi17 commands from lonertic

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.7.54"
-__version_comment__ = "recent changes: add jk rs232/485 protocol"
+__version__ = "0.7.55"
+__version_comment__ = "recent changes: add extra pi17 commands from lonertic"
