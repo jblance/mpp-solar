584b863abaf980847585eae0eb3a22ef0fe111d5
John Blance <john.blance@gmail.com>
2021-11-12 16:23:40 +1300

    move port_types to enum

--- mppsolar/io/__init__.py
+++ mppsolar/io/__init__.py
@@ -1,66 +1,71 @@
 # import importlib
 import logging
-from ..helpers import get_kwargs
+from enum import Enum, auto
 
-PORT_TYPE_UNKNOWN = 0
-PORT_TYPE_TEST = 1
-PORT_TYPE_USB = 2
-PORT_TYPE_ESP32 = 4
-PORT_TYPE_SERIAL = 8
-PORT_TYPE_JKBLE = 16
-PORT_TYPE_MQTT = 32
-PORT_TYPE_VSERIAL = 64
-PORT_TYPE_DALYSERIAL = 128
+from mppsolar.helpers import get_kwargs
+
+
+class PortType(Enum):
+    UNKNOWN = auto()
+    TEST = auto()
+    USB = auto()
+    ESP32 = auto()
+    SERIAL = auto()
+    JKBLE = auto()
+    MQTT = auto()
+    VSERIAL = auto()
+    DALYSERIAL = auto()
+
 
 log = logging.getLogger("io")
 
 
 def get_port_type(port):
     if port is None:
-        return PORT_TYPE_UNKNOWN
+        return PortType.UNKNOWN
 
     port = port.lower()
     # check for test type port
     if "test" in port:
         log.debug("port matches test")
-        return PORT_TYPE_TEST
+        return PortType.TEST
     # mqtt
     elif "mqtt" in port:
         log.debug("port matches mqtt")
-        return PORT_TYPE_MQTT
+        return PortType.MQTT
     # USB type ports
     elif "hidraw" in port:
         log.debug("port matches hidraw")
-        return PORT_TYPE_USB
+        return PortType.USB
     elif "mppsolar" in port:
         log.debug("port matches mppsolar")
-        return PORT_TYPE_USB
+        return PortType.USB
     # ESP type ports
     elif "esp" in port:
         log.debug("port matches esp")
-        return PORT_TYPE_ESP32
+        return PortType.ESP32
     # JKBLE type ports
     elif ":" in port:
         # all mac addresses currently return as JKBLE
         log.debug("port matches jkble ':'")
-        return PORT_TYPE_JKBLE
+        return PortType.JKBLE
     elif "jkble" in port:
         log.debug("port matches jkble")
-        return PORT_TYPE_JKBLE
+        return PortType.JKBLE
     elif "daly" in port:
         log.debug("port matches daly")
-        return PORT_TYPE_DALYSERIAL
+        return PortType.DALYSERIAL
     elif "vserial" in port:
         log.debug("port matches vserial")
-        return PORT_TYPE_VSERIAL
+        return PortType.VSERIAL
     elif "serial" in port:
         log.debug("port matches serial")
-        return PORT_TYPE_SERIAL
+        return PortType.SERIAL
     elif "ttyusb" in port:
         log.debug("port matches ttyusb")
-        return PORT_TYPE_SERIAL
+        return PortType.SERIAL
     else:
-        return PORT_TYPE_UNKNOWN
+        return PortType.UNKNOWN
 
 
 def get_port(*args, **kwargs):
@@ -74,49 +79,49 @@ def get_port(*args, **kwargs):
     else:
         port_type = get_port_type(port)
 
-    if port_type == PORT_TYPE_TEST:
+    if port_type == PortType.TEST:
         log.info("Using testio for communications")
         from mppsolar.io.testio import TestIO
 
         _port = TestIO(device_path=port)
 
-    elif port_type == PORT_TYPE_USB:
+    elif port_type == PortType.USB:
         log.info("Using hidrawio for communications")
         from mppsolar.io.hidrawio import HIDRawIO
 
         _port = HIDRawIO(device_path=port)
 
-    elif port_type == PORT_TYPE_ESP32:
+    elif port_type == PortType.ESP32:
         log.info("Using esp32io for communications")
         from mppsolar.io.esp32io import ESP32IO
 
         _port = ESP32IO(device_path=port)
 
-    elif port_type == PORT_TYPE_JKBLE:
+    elif port_type == PortType.JKBLE:
         log.info("Using jkbleio for communications")
         from mppsolar.io.jkbleio import JkBleIO
 
         _port = JkBleIO(device_path=port)
 
-    elif port_type == PORT_TYPE_SERIAL:
+    elif port_type == PortType.SERIAL:
         log.info("Using serialio for communications")
         from mppsolar.io.serialio import SerialIO
 
         _port = SerialIO(device_path=port, serial_baud=baud)
 
-    elif port_type == PORT_TYPE_DALYSERIAL:
+    elif port_type == PortType.DALYSERIAL:
         log.info("Using dalyserialio for communications")
         from mppsolar.io.dalyserialio import DalySerialIO
 
         _port = DalySerialIO(device_path=port, serial_baud=baud)
 
-    elif port_type == PORT_TYPE_VSERIAL:
+    elif port_type == PortType.VSERIAL:
         log.info("Using vserialio for communications")
         from mppsolar.io.vserialio import VSerialIO
 
         _port = VSerialIO(device_path=port, serial_baud=baud, records=30)
 
-    elif port_type == PORT_TYPE_MQTT:
+    elif port_type == PortType.MQTT:
         mqtt_broker = get_kwargs(kwargs, "mqtt_broker", "localhost")
         name = get_kwargs(kwargs, "name", "unnamed")
         # mqtt_port = get_kwargs(kwargs, "mqtt_port", 1883)
