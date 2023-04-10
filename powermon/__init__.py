e8a2f10293e87476f9f06aedf1c9c4e054afa25c
Ross Andrews <rossmandrews@gmail.com>
2023-04-10 16:42:13 -0700

    Fixing docker compose after merge

--- powermon/__init__.py
+++ powermon/__init__.py
@@ -7,11 +7,11 @@ from time import sleep, time
 
 import yaml
 
-from mppsolar.libs.mqttbrokerc import MqttBroker
-from mppsolar.libs.daemon import Daemon
-from mppsolar.ports import get_port
 from mppsolar.protocols import get_protocol
 from mppsolar.version import __version__  # noqa: F401
+from powermon.libs.daemon import Daemon
+from powermon.libs.mqttbroker import MqttBroker
+from powermon.ports import get_port
 
 from mppsolar.libs.schedule import Schedule, LoopCommandSchedule, CommandScheduleType, Command
 
--- powermon/transports/__init__.py
+++ powermon/transports/__init__.py
@@ -1,7 +1,7 @@
 import logging
-from mppsolar.sender.screen import Screen
-from mppsolar.sender.mqtt import MQTT
-from mppsolar.sender.mqtt_html import MQTT_HTML
+from powermon.transports.screen import Screen
+from powermon.transports.mqtt import MQTT
+from powermon.transports.mqtt_html import MQTT_HTML
 from enum import StrEnum, auto
 
 class OutputType(StrEnum):
@@ -29,39 +29,3 @@ def get_output(outputType, mqtt_broker, topic, tag):
 
     return output_class
 
-
-#def output_results(results, command, mqtt_broker, fullconfig={}):
-    # "normal command definition"
-    # - command: QPIGS
-    #   outputs:
-    #   - name: screen
-    #     format: raw
-    #     tag: Test_Inverter
-    #     filter: ^serial|^work|^charger|^fault
-    #   - name: mqtt
-    #     results_topic: results/qpigs
-    #
-    # or "adhoc_commands"
-    #  topic: test/command_topic
-    #  outputs:
-    #  - name: screen
-
-    # Check if an outputs section was supplied
-#    if "outputs" in command:
-#        outputs = command["outputs"]
-    # if not default to screen
-#    else:
-#        outputs["name"] = "screen"
-#    for op in outputs:
-#        # filter = config.get("CONFIG", "filter")
-#        # log.debug(f"Using output filter: {filter}")
-#        if "name" in op:
-#            output = get_output(op["name"])
-#        else:
-#            output = get_output("screen")
-#        output.output(
-#            data=dict(results),
-#            config=op,
-#            mqtt_broker=mqtt_broker,
-#            fullconfig=fullconfig,
-#        )
