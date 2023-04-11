1550870a3926ef3a4ac4d97f3c2719773bf624cf
Ross Andrews <rossmandrews@gmail.com>
2023-04-11 15:53:29 -0700

    Getting the development docker commands to work in the make file. Adding USBPort so I can test with my hardware

--- mppsolar/version.py
+++ mppsolar/version.py
@@ -1,5 +1,7 @@
-import configparser
+#import configparser
 
-version = configparser.ConfigParser()
-version.read("pyproject.toml")
-__version__ = version["tool.poetry"]["version"].replace('"', "")
+#version = configparser.ConfigParser()
+#version.read("pyproject.toml")
+#__version__ = version["tool.poetry"]["version"].replace('"', "")
+
+__version__ = "0.15.30"
