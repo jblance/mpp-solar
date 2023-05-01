eb5aa250999a9b3f0b780ad68cce7fe582a30b87
jblance <john.blance@gmail.com>
2023-05-02 11:18:27 +1200

    add defaults to device

--- powermon/libs/device.py
+++ powermon/libs/device.py
@@ -1,16 +1,8 @@
 """device.py"""
-
 import logging
-import sys
-
-
-# from mppsolar.protocols import get_protocol
-from powermon.ports import getPortFromConfig
 
 from dto.deviceDTO import DeviceDTO
-
-# from powermon.libs.errors import ConfigError
-
+from powermon.ports import getPortFromConfig
 
 # Set-up logger
 log = logging.getLogger("Device")
@@ -26,18 +18,17 @@ class Device:
     also contains the name, model and id of the device
     """
 
-    def __init__(self, config=None):
+    def __init__(self, config):
         if not config:
-            log.error("No device definition in config. Check configFile?")
-            sys.exit(1)
+            log.warning("No device definition in config. Check configFile?")
+            config = {}
+            # sys.exit(1)
         self.name = config.get("name", "mppsolar")
         self.identifier = config.get("identifier", "mppsolar")
         self.model = config.get("model", "mppsolar")
         self.manufacturer = config.get("manufacturer", "mppsolar")
-        self.port = getPortFromConfig(config.get("port", None))
 
-        # self.protocol = get_protocol(config.get("protocol", None))  # TODO: move to port
-
+        self.port = getPortFromConfig(config.get("port"))
         # error out if unable to configure port
         if not self.port:
             log.error("Invalid port config '%s' found", config)
