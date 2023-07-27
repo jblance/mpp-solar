5c9a3814cdf90a39f36fa0807dfb770905ea3fc5
John Blance <1266998+jblance@users.noreply.github.com>
2023-07-27 21:54:30 +0000

    fix pi17m058 test

--- tests/test_protocol_pi17m058.py
+++ tests/test_protocol_pi17m058.py
@@ -4,7 +4,27 @@ import subprocess
 
 QUERY_COMMANDS = [
     ("PI", "protocol_version=17\n"),
-    ("BATS", """\n"""),
+    ("BATS", """battery_maximum_charge_current=175.0(A)
+battery_constant_charge_voltage=56.0(V)
+battery_floating_charge_voltage=54.0(V)
+battery_stop_charger_current_level_in_floating_charging=0.0(A)
+keep_charged_time_of_battery_catch_stopped_charging_current_level=60(Minutes)
+battery_voltage_of_recover_to_charge_when_battery_stop_charger_in_floating_charging=53.0(V)
+battery_under_voltage=42.0(V)
+battery_under_voltage_release=48.0(V)
+battery_weak_voltage_in_hybrid_mode=48.0(V)
+battery_weak_voltage_release_in_hybrid_mode=54.0(V)
+battery_type=Ordinary
+ac_charger_keep_battery_voltage_function_enable/diable=Disabled
+ac_charger_keep_battery_voltage=48.0(V)
+battery_temperature_sensor_compensation=0.0(mV)
+max._ac_charging_current=10.0(A)
+battery_discharge_max_current_in_hybrid_mode=175(A)
+battery_under_soc=10(%)
+battery_under_back_soc=20(%)
+battery_weak_soc_in_hybrid_mode=20(%)
+battery_weak_back_soc_in_hybrid_mode=80(%)
+unknown=0\n"""),
 ]
 # ("", """\n"""),
 # ("DI", """\n"""),
@@ -31,7 +51,7 @@ def do_test(self, command, expected, respno=0):
             text=True,
         )
 
-        print(result.stdout)
+        # print(result.stdout)
         # print(result.stderr)
         # print(".")
         self.assertEqual(f"CMD: {command}\n{result.stdout}", f"CMD: {command}\n{expected}")
