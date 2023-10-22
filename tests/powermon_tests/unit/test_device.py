6630b1c70e3022f9d76f827eae8dc6574b5a74e2
John Blance <1266998+jblance@users.noreply.github.com>
2023-10-22 03:07:25 +0000

    update test for looping change

--- tests/powermon_tests/unit/test_device.py
+++ tests/powermon_tests/unit/test_device.py
@@ -6,33 +6,27 @@ from powermon.commands.command import Command
 from powermon.outputs.abstractoutput import AbstractOutput
 from powermon.protocols.pi30max import pi30max
 
-class DeviceTest(TestCase):
 
+class DeviceTest(TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
         self.port = None
         self.device = None
 
     def setUp(self) -> None:
-        self.port = Mock(spec=SerialPort, protocol = pi30max())
+        self.port = Mock(spec=SerialPort, protocol=pi30max())
         self.device = Device(name="Test Device", port=self.port)
 
-    def test_if_output_processed_in_success_runLoop(self):
+    def test_if_output_processed_in_success_run(self):
         """
         Test if output process called in success command run
         """
 
         # Add command into command list with dueToRun=True to emulate running command
         output = Mock(spec=AbstractOutput)
-        self.device.add_command(
-            Mock(
-                spec=Command, 
-                code="QPIRI",
-                outputs = [output],
-                dueToRun = Mock(return_value=True)
-                )
-            )
-    
-        #Run main device loop. Expecting positive result
-        self.assertTrue(self.device.run())
+        self.device.add_command(Mock(spec=Command, code="QPIRI", outputs=[output], dueToRun=Mock(return_value=True)))
+
+        # Run main device loop. Expecting positive result
+        # self.assertTrue(self.device.run())
+        self.device.run()
         output.process.assert_called()
