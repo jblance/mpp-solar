21b915e83ab3da0e4d3c1d55d6d816f9c944a1ff
John Blance <1266998+jblance@users.noreply.github.com>
2024-02-01 23:00:29 +0000

    rename overloaded filter var

--- mppsolar/outputs/screen.py
+++ mppsolar/outputs/screen.py
@@ -1,3 +1,4 @@
+""" mppsolar / outputs / screen.py """
 import logging
 import re
 
@@ -8,13 +9,14 @@ log = logging.getLogger("screen")
 
 
 class screen(baseoutput):
+    """ the default output model, outputs to standard out in a table format """
     def __str__(self):
         return "[the default output module] outputs the results to standard out in a slightly formatted way"
 
     def __init__(self, *args, **kwargs) -> None:
-        log.debug(f"processor.screen __init__ kwargs {kwargs}")
+        log.debug(f"processor.screen __init__ args: {args}, kwargs: {kwargs}")
 
-    def printHeader(command, description):
+    def printHeader(self, command, description):
         pass
 
     def output(self, *args, **kwargs):
@@ -31,17 +33,17 @@ class screen(baseoutput):
             # get formatting info
             remove_spaces = config.get("remove_spaces", True)
             keep_case = config.get("keep_case", False)
-            filter = config.get("filter", None)
+            _filter = config.get("filter", None)
             excl_filter = config.get("excl_filter", None)
         else:
             # get formatting info
             remove_spaces = True
             keep_case = get_kwargs(kwargs, "keep_case")
-            filter = get_kwargs(kwargs, "filter")
+            _filter = get_kwargs(kwargs, "filter")
             excl_filter = get_kwargs(kwargs, "excl_filter")
 
-        if filter is not None:
-            filter = re.compile(filter)
+        if _filter is not None:
+            _filter = re.compile(filter)
         if excl_filter is not None:
             excl_filter = re.compile(excl_filter)
 
@@ -61,7 +63,7 @@ class screen(baseoutput):
             if not keep_case:
                 # make lowercase
                 key = key.lower()
-            if key_wanted(key, filter, excl_filter):
+            if key_wanted(key, _filter, excl_filter):
                 displayData[key] = _values
         log.debug(f"displayData: {displayData}")
 
