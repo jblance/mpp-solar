5ec16d35a143d63f514d05a0ca1b676dc5a7246e
John Blance <1266998+jblance@users.noreply.github.com>
2024-02-15 09:31:25 +0000

    remove pi30 inheritance from pi18

--- powermon/protocols/pi18.py
+++ powermon/protocols/pi18.py
@@ -5,7 +5,8 @@ from powermon.commands.command_definition import CommandDefinition
 from powermon.commands.reading_definition import ReadingType, ResponseType
 from powermon.commands.result import ResultType
 from powermon.ports.porttype import PortType
-from powermon.protocols.pi30 import BATTERY_TYPE_LIST, OUTPUT_MODE_LIST, PI30
+from powermon.protocols.pi30 import BATTERY_TYPE_LIST, OUTPUT_MODE_LIST
+from powermon.protocols.abstractprotocol import AbstractProtocol
 
 log = logging.getLogger("pi18")
 
@@ -72,10 +73,10 @@ QUERY_COMMANDS = {
     },
 }
 
-COMMANDS_TO_REMOVE = ["QPIRI"]
+COMMANDS_TO_REMOVE = []
 
 
-class PI18(PI30):
+class PI18(AbstractProtocol):
     """ pi18 protocol handler """
     def __str__(self):
         return "PI18 protocol handler"
@@ -86,12 +87,12 @@ class PI18(PI30):
         self.add_command_definitions(QUERY_COMMANDS)
         self.add_command_definitions(SETTER_COMMANDS, result_type=ResultType.ACK)
         self.remove_command_definitions(COMMANDS_TO_REMOVE)
-        self.check_definitions_count(expected=44)
+        self.check_definitions_count(expected=1)
         self.add_supported_ports([PortType.SERIAL, PortType.USB])
 
     def check_crc(self, response: str, command_definition: CommandDefinition = None):
         """ crc check, override for now """
-        log.debug("check crc for %s in pi30", response)
+        log.debug("check crc for %s in pi18", response)
         return True
 
     def trim_response(self, response: str, command_definition: CommandDefinition = None) -> str:
@@ -107,3 +108,74 @@ class PI18(PI30):
             # pi30 style response
             response = response[1:]
         return response
+
+    def get_full_command(self, command: str) -> bytes:
+        """ generate the full command including crc and \n as needed """
+        log.info("Using protocol: %s with %i commands", self.protocol_id, len(self.command_definitions))
+        # byte_cmd = bytes(command, "utf-8")
+        # # calculate the CRC
+        # crc_high, crc_low = crc(byte_cmd)
+        # # combine byte_cmd, CRC , return
+        # full_command = byte_cmd + bytes([crc_high, crc_low, 13])
+        # log.debug("full command: %s", full_command)
+        # return full_command
+
+        # """
+        # Override the default get_full_command as its different
+        # """
+        # log.info(f"Using protocol {self._protocol_id} with {len(self.COMMANDS)} commands")
+        # # These need to be set to allow other functions to work`
+        # self._command = command
+        # self._command_defn = self.get_command_defn(command)
+        # # End of required variables setting
+        # if self._command_defn is None:
+        #     return None
+
+        # # Full command components
+        # _cmd = bytes(self._command, "utf-8")
+        # log.debug(f"_cmd is: {_cmd}")
+
+        # _type = self._command_defn["type"]
+        # log.debug(f"_type is: {_type}")
+
+        # # Hand coded prefix
+        # _prefix = self._command_defn["prefix"]
+        # log.debug(f"_prefix: {_prefix}")
+        # # Auto determined prefix - TODO
+        # data_length = len(_cmd) + 3
+        # if _type == "QUERY":
+        #     auto_prefix = f"^P{data_length:03}"
+        # elif _type == "SETTER":
+        #     auto_prefix = f"^S{data_length:03}"
+        # else:
+        #     log.info(f"No type defined for command {_cmd}")
+        #     auto_prefix = f"^P{data_length:03}"
+        # log.debug(f"auto_prefix: {auto_prefix}")
+
+        # _pre_cmd = bytes(_prefix, "utf-8") + _cmd
+        # # _pre_cmd = bytes(auto_prefix, "utf-8") + _cmd
+        # log.debug(f"_pre_cmd: {_pre_cmd}")
+
+        # # Determine if crc is needed or not
+        # CRC = True
+        # # For commands that dont need CRC
+        # if self._command_defn.get("nocrc") is True:
+        #     CRC = False
+        # # for protocols that mostly dont need CRC
+        # if self.NOCRC:
+        #     CRC = False
+        # # override to allow crc
+        # if self._command_defn.get("nocrc") is False:
+        #     CRC = True
+        # log.debug("CRC: %s" % CRC)
+
+        # if CRC:
+        #     # calculate the CRC
+        #     crc_high, crc_low = crc(_pre_cmd)
+        #     # combine byte_cmd, CRC , return
+        #     full_command = _pre_cmd + bytes([crc_high, crc_low, 13])
+        # else:
+        #     full_command = _pre_cmd + bytes([13])
+
+        # log.debug(f"full command: {full_command}")
+        # return full_command
