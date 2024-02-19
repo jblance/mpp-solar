dc76b99884b77c2ea855287eda7e471b63b2487c
John Blance <1266998+jblance@users.noreply.github.com>
2024-02-19 06:54:14 +0000

    tweak exceptions to expose problems

--- powermon/ports/bleport.py
+++ powermon/ports/bleport.py
@@ -8,6 +8,7 @@ from bleak import BleakClient
 from powermon.commands.command import Command, CommandType
 from powermon.commands.result import Result
 from powermon.dto.portDTO import PortDTO
+from powermon.errors import PowermonWIP
 from powermon.ports.abstractport import AbstractPort
 from powermon.ports.porttype import PortType
 from powermon.protocols import get_protocol_definition
@@ -82,6 +83,7 @@ class BlePort(AbstractPort):
             # self.error_message = str(e)
             print(e)
             self.client = None
+            raise PowermonWIP("connect failed") from e
         
         return self.is_connected()
 
@@ -93,61 +95,61 @@ class BlePort(AbstractPort):
 
     async def send_and_receive(self, command: Command) -> Result:
         full_command = command.full_command
-        command_code=90
+        command_code = 90
         response_line = None
         log.debug("port: %s, full_command: %s", self.client, full_command)
         if not self.is_connected():
             raise RuntimeError("Ble port not open")
-        try:
-            log.debug("Executing command via ble...")
-            await self.client.write_gatt_char(15, full_command)
-            log.debug("Waiting...")
-            # try:
-            response_line = await asyncio.wait_for(self.response_cache[command_code]["future"], 5)
-            # except asyncio.TimeoutError:
-                # log.warning("Timeout while waiting for %s response" % command)
-                # return False
-            print("got %s" % response_line)
-            #return result
-            # self.serial_port.reset_input_buffer()
-            # self.serial_port.reset_output_buffer()
-            # # Process i/o differently depending on command type
-            # command_defn = command.command_definition
-            # match command_defn.command_type:
-            #     case CommandType.VICTRON_LISTEN:
-            #         # this command type doesnt need to send a command, it just listens on the serial port
-            #         _lines = 30
-            #         log.debug("VictronCommandType.LISTEN s&r, listening for %i lines", _lines)
-            #         response_line = b""
-            #         for _ in range(_lines):
-            #             _response = self.serial_port.read_until(b"\n")
-            #             response_line += _response
-            #     case CommandType.SERIAL_READONLY:
-            #         # read until no more data
-            #         log.debug("ReadAll s&r")
-            #         response_line = b""
-            #         while True:
-            #             time.sleep(0.2)  # give serial port time to receive the data
-            #             to_read = self.serial_port.in_waiting
-            #             log.debug("bytes waiting: %s", to_read)
-            #             if to_read == 0:
-            #                 break
-            #             # got some data to read
-            #             response_line += self.serial_port.read(to_read)
-            #     case _:
-            #         # default processing
-            #         c = self.serial_port.write(full_command)
-            #         log.debug("Default serial s&r. Wrote %i bytes", c)
-            #         self.serial_port.flush()
-            #         time.sleep(0.1)  # give serial port time to receive the data
-            #         response_line = self.serial_port.read_until(b"\r")
-            log.debug("serial response was: %s", response_line)
-            # response = self.get_protocol().check_response_and_trim(response_line)
-            result = command.build_result(raw_response=response_line, protocol=self.protocol)
-            return result
-        except Exception as e:
-            log.warning("Serial read error: %s", e)
-            result.error = True
-            result.error_messages.append(f"Serial read error {e}")
-            self.disconnect()
-            return result
+        # try:
+        log.debug("Executing command via ble...")
+        await self.client.write_gatt_char(15, full_command)
+        log.debug("Waiting...")
+        # try:
+        response_line = await asyncio.wait_for(self.response_cache[command_code]["future"], 5)
+        # except asyncio.TimeoutError:
+            # log.warning("Timeout while waiting for %s response" % command)
+            # return False
+        print("got %s" % response_line)
+        #return result
+        # self.serial_port.reset_input_buffer()
+        # self.serial_port.reset_output_buffer()
+        # # Process i/o differently depending on command type
+        # command_defn = command.command_definition
+        # match command_defn.command_type:
+        #     case CommandType.VICTRON_LISTEN:
+        #         # this command type doesnt need to send a command, it just listens on the serial port
+        #         _lines = 30
+        #         log.debug("VictronCommandType.LISTEN s&r, listening for %i lines", _lines)
+        #         response_line = b""
+        #         for _ in range(_lines):
+        #             _response = self.serial_port.read_until(b"\n")
+        #             response_line += _response
+        #     case CommandType.SERIAL_READONLY:
+        #         # read until no more data
+        #         log.debug("ReadAll s&r")
+        #         response_line = b""
+        #         while True:
+        #             time.sleep(0.2)  # give serial port time to receive the data
+        #             to_read = self.serial_port.in_waiting
+        #             log.debug("bytes waiting: %s", to_read)
+        #             if to_read == 0:
+        #                 break
+        #             # got some data to read
+        #             response_line += self.serial_port.read(to_read)
+        #     case _:
+        #         # default processing
+        #         c = self.serial_port.write(full_command)
+        #         log.debug("Default serial s&r. Wrote %i bytes", c)
+        #         self.serial_port.flush()
+        #         time.sleep(0.1)  # give serial port time to receive the data
+        #         response_line = self.serial_port.read_until(b"\r")
+        log.debug("serial response was: %s", response_line)
+        # response = self.get_protocol().check_response_and_trim(response_line)
+        result = command.build_result(raw_response=response_line, protocol=self.protocol)
+        return result
+        # except Exception as e:
+        #     log.warning("Serial read error: %s", e)
+        #     result.error = True
+        #     result.error_messages.append(f"Serial read error {e}")
+        #     self.disconnect()
+        #     return result
