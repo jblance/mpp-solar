c7096fd1c2f8f013a8c9545ef63ce6669209c347
John Blance <1266998+jblance@users.noreply.github.com>
2024-02-25 06:56:44 +0000

    try write to keep alive

--- utils/victron-ble.py
+++ utils/victron-ble.py
@@ -10,6 +10,8 @@ async def main():
         if (not client.is_connected):
             raise Exception("client not connected")
 
+        await client.write_gatt_char('6597ffff-4bda-4c1e-af4b-551c4cf74769', 0x30)
+
         char = await client.read_gatt_char('65970fff-4bda-4c1e-af4b-551c4cf74769')
         print(char)
 
